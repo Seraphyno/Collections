df70f5b454f8af8f67f62407862a72ab08fa2d43
Serafino Tudorovici <serafino.radulescu@gmail.com>
2019-05-22 11:15:54 +0300

    H

--- src/main/java/com/sda/collections/Tema.java
+++ src/main/java/com/sda/collections/Tema.java
@@ -9,13 +9,13 @@ import java.util.Map;
  * si de cate ori
  */
 public class Tema {
-    
+
     public static void main(String[] args) {
         String[] cuvinte = cleanUpText().split(" ");
         Map<String, Integer> dictionar = new HashMap<String, Integer>();
-        
+
         System.out.println(cuvinte.length);
-        
+
         for (String cuvant : cuvinte) {
             if (dictionar.containsKey(cuvant)) {
                 Integer occurences = dictionar.get(cuvant);
@@ -24,25 +24,25 @@ public class Tema {
                 dictionar.put(cuvant, 1);
             }
         }
-    
+
         printMap(dictionar);
     }
-    
+
     private static void printMap(Map<String, Integer> dictionar) {
         for (String key : dictionar.keySet()) {
             System.out.println("[" + key + ", " + dictionar.get(key) + "]");
         }
     }
-    
+
     private static String cleanUpText() {
         return TextTema.TEXT.toLowerCase()
-                    .replace(",","")
-                    .replace("?"," ")
-                    .replace("."," ")
-                    .replace("  ", " ")
-                    .replace("\\\"", "")
-                    .replace("(", "")
-                    .replace(")", "")
-                    .trim();
+                .replace(",", "")
+                .replace("?", " ")
+                .replace(".", " ")
+                .replace("  ", " ")
+                .replace("\\\"", "")
+                .replace("(", "")
+                .replace(")", "")
+                .trim();
     }
 }
